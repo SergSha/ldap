8580b32a73fea2e31e33df8a1108099495047481
Serg Shagalov <sy.shagalov@yandex.ru>
2022-11-01 16:50:55 +0300

    Modify script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir ~/.ssh && ssh-keygen -q -t rsa -b 4096 -f ~/.ssh/ipetrov -N '' && cp ~/.ssh/ipetrov.pub ./ansible/roles/ldap/files/ -f && vagrant up
+mkdir ~/.ssh; ssh-keygen -q -t rsa -b 4096 -f ~/.ssh/ipetrov -N '' && cp ~/.ssh/ipetrov.pub ./ansible/roles/ldap/files/ -f && vagrant up
